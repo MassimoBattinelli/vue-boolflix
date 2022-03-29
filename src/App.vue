7cc91f66fe4fcf09b8e00a7a47fd61b1e256b204
massi <massimo.battinelli@gmail.com>
2022-03-29 17:10:10 +0200

    bandiere lingua (da fixare)

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <HeaderBool @cards-received="arrMovies = $event" />
     <MainBool :arr-movie-name="arrMovies" />
+    <LangFlag />
   </div>
 </template>
 
 <script>
 import HeaderBool from "./components/HeaderBool.vue";
 import MainBool from "./components/MainBool.vue";
+import LangFlag from "./main.js";
 
 export default {
   name: "App",
   components: {
     HeaderBool,
     MainBool,
+    LangFlag,
   },
   data() {
     return {
